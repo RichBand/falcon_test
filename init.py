5110337b9de2a6d47711416dfadf64ca3cfc808a
RichBand <ricardobandala@gmail.com>
2017-06-28 13:05:34 -0400

    work on_get method

--- init.py
+++ init.py
@@ -12,13 +12,25 @@ from wsgiref.simple_server import make_server
 
 class CharacterCollection(object):
 
-    @staticmethod
-    def on_get(req, resp):
+    def on_get(self, req, resp):
         """Handles GET requests"""
-        characters = req.conn.execute('SELECT * FROM characters;').fetchall()
+        _field_names = ('name', 'sex', 'hair', 'alive')
+        param_fields = list(req.params.keys())
+
+        if not set(param_fields).issubset(_field_names):
+            raise falcon.HTTPBadRequest(
+                'Bad Request',
+                'invalid parameters: ' + ', '.join(set(param_fields) - set(_field_names))
+            )
+        elif req.params:
+            characters = req.cur.execute(
+                'SELECT * FROM characters where ' + self.get_params(param_fields),
+                req.params).fetchall()
+        elif not req.params:
+            characters = req.cur.execute('SELECT * FROM characters;').fetchall()
+
         resp.body = json.dumps(characters)
 
-    @staticmethod
     def on_post(self, req, resp):
         data = json.loads(req.stream.read())
         errors = self.validate_character(data)
@@ -27,24 +39,27 @@ class CharacterCollection(object):
             raise falcon.HTTPBadRequest('Bad Request', ''.join(errors))
         else:
             try:
-                req.conn.execute(
-                    'INSERT INTO characters (id, name, gender, hair, alive) values (?, ?, ?, ?, ?)',
-                    (None, data['name'], data['gender'], data['hair'], data['alive']))
-
+                req.cur.execute(
+                    'INSERT INTO characters (id, name, sex, hair, alive) values (?, ?, ?, ?, ?)',
+                    (None, data['name'], data['sex'], data['hair'], data['alive']))
             except ValueError:
                 raise falcon.HTTPError(falcon.HTTP_202, 'Error', )
-
+        req.conn.commit()
         resp.status = falcon.HTTP_202
 
     @staticmethod
-    def validate_character(self, data):
+    def validate_character(data):
         _hair = ('yellow', 'blue', 'brown', 'black', 'white', 'bald')
-        _gender = ('male', 'female', 'undefined')
+        _sex = ('male', 'female', 'undefined')
         errors = []
-        errors.append('name can\'t be empty') if data['name'] not in _hair else None
-        errors.append('wrong hair type') if data['hair'] not in _hair else None
-        errors.append('wrong gender type') if data['gender'] not in _gender else None
+        errors.append('name can\'t be empty') if not len(data['name']) else None
+        errors.append('wrong hair type') if 'hair' not in data or data['hair'] not in _hair else None
+        errors.append('wrong sex type') if 'sex' not in data or data['sex'] not in _sex else None
         return errors
+
+    @staticmethod
+    def get_params(params: list):
+        return 'and '.join('{0}=:{0} '.format(param) for param in params)
 
 
 class ContentComponent:
@@ -61,15 +76,18 @@ class DatabaseComponent:
     def __init__(self):
         self.conn = sqlite3.connect("Simpsons.db")
         cursor = self.conn.cursor()
-        cursor.execute('CREATE TABLE IF NOT EXISTS characters (id INTEGER PRIMARY KEY, name VARCHAR(20), gender VARCHAR(20), hair VARCHAR(20), alive BOOLEAN )' )
+        cursor.execute('CREATE TABLE IF NOT EXISTS characters (id INTEGER PRIMARY KEY, name VARCHAR(20), sex VARCHAR(20), hair VARCHAR(20), alive BOOLEAN )' )
         self.conn.commit()
+
         cursor.close()
 
     def process_request(self, req, resp):
-        req.conn = self.conn.cursor()
+        req.cur = self.conn.cursor()
+        req.conn = self.conn
 
     def process_response(self, req, resp, resource, req_succeeded):
-        req.conn.close()
+        pass
+        # req.conn.close()
 
 
 class Application(falcon.API):
--- quick_queries.py
+++ quick_queries.py
@@ -3,9 +3,10 @@ import sqlite3, json
 def Main():
     conn = sqlite3.connect('Simpsons.db')
     cur = conn.cursor()
-    result = cur.execute('SELECT * FROM characters').fetchall()
+    # result = cur.execute('SELECT * FROM characters').fetchall()
     # result = cur.execute('DELETE FROM characters WHERE id = 1;').fetchall()
-    result = json.dumps(result)
+    result = cur.execute('DROP TABLE characters')
+    # result = json.dumps(result)
     print(result)
     conn.commit()
     cur.close()
